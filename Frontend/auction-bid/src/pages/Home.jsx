48ea46b9a2d6cbaff02039140aa243a89010f808
Pravin <pravinpagar290@gmail.com>
2026-01-17 23:43:21 +0530

    polish home page ui

--- Frontend/auction-bid/src/pages/Home.jsx
+++ Frontend/auction-bid/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
       <section className="bg-white">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-900">
+            <h1 className="max-w-2xl mb-4 text-4xl font-light tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-900">
               Discover Unique Treasures & Bid in Real-Time
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
@@ -31,7 +31,7 @@ const Home = () => {
             <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
               <Link
                 to="/auctions"
-                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300"
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-black hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300"
               >
                 Start Bidding
                 <svg
